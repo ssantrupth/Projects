7cfc7bcb31886fcd51992a2430aff90285c13f1d
Santrupth Sunkari <ssunkari@asu.edu>
2024-12-26 12:16:52 -0700

    Setup.py

---
+++ mlprojects/Project_1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)-> list[str]:
+    '''
+    this function will return the list of requirements '''
+    with open(file_path, 'r') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+    name='mlproject1',
+    version='0.0.1',
+    description='My ML Projects',
+    author='ssantrupth',
+    author_email='santrupthsunkari@gmail.com',
+    url='https://github.com/ssantrupth/Projects.git',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=get_requirements('requirements.txt'),
+)
